4a5f8ca3aa21ce7a570f3c9d07091c3605ca5d7c
p121s <p121s@mail.ru>
2021-10-22 13:37:17 +0300

    Add action constants

--- src/constants/actionsConctants.ts
+++ src/constants/actionsConctants.ts
@@ -2,3 +2,6 @@ export const SET_NAME = 'SET_NAME';
 export const SET_ID_USER = 'SET_ID_USER';
 export const DELETE_NAME = 'DELETE_NAME';
 export const DELETE_ID_USER = 'DELETE_ID_USER';
+
+export const GET_USER_IMAGES = 'GET_USER_IMAGES';
+export const GET_ALL_IMAGES = 'GET_ALL_IMAGES';
