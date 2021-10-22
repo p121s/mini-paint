03d144b1c1c67165fb0f68664dd687ffb93bb578
p121s <p121s@mail.ru>
2021-10-22 13:35:26 +0300

    Create second reducer

---
+++ src/redux/reducers/reducerImageDate.ts
@@ -0,0 +1,17 @@
+import { GET_USER_IMAGES, GET_ALL_IMAGES } from "../../constants/actionsConctants";
+
+const INIT_STATE = {
+    userImages: [],
+    allImages: [],
+};
+
+export const reducerImageDate = (state = INIT_STATE, action: any) => {
+    switch(action.type) {
+        case GET_USER_IMAGES:
+            return {...state, userImages: [...action.payload]};
+        case GET_ALL_IMAGES:
+            return {...state, allImages: [...action.payload]};
+        default:
+            return state;
+    }
+}
--- src/redux/reducers/rootReducer.ts
+++ src/redux/reducers/rootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer } from './reducer';
+import { reducerImageDate } from './reducerImageDate';
 
 
 export const rootReducer = combineReducers({
     reduce: reducer,
+    reduceImages: reducerImageDate,
 });
