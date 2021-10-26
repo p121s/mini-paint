24211ce034fb464697ce816cf5d37088070d9439
p121s <p121s@mail.ru>
2021-10-26 16:48:59 +0300

    Fix code styled components

--- src/styledComponents/blocks/EditorModalBlock.ts
+++ src/styledComponents/blocks/EditorModalBlock.ts
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
 
-interface IsColorBlock {
+interface isEditorModalBlock {
   readonly isBlock: boolean;
 }
 
-export const ColorBlock = styled.div<IsColorBlock>`
-  width: 70%;
+export const EditorModalBlock = styled.div<isEditorModalBlock>`
+  width: 60%;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   display: ${({isBlock}) => isBlock ? 'block' : 'none'};
-  background-color: rgb(240, 240, 240);
-  border: 1px solid lightgray;
   padding: 50px;
+  background-color: rgb(240, 240, 240);
   text-align: center;
+  border: 1px solid lightgray;
 `;
